5b65f50783ab87c20295114c5735dcf822872c83
马华标(060984) <huabiao.mahb@alibaba-inc.com>
2014-09-03 21:14:06 +0800

    key update

--- store-portal/src/main/java/com/graby/store/portal/web/TopAuthController.java
+++ store-portal/src/main/java/com/graby/store/portal/web/TopAuthController.java
@@ -34,10 +34,10 @@ public class TopAuthController {
 	// Sandbox 1021812303, sandbox435b20fcc2b863c1126e473e5
 
 	@Value("${top.appkey}")
-	private final String clientId = "23012748";
+	private final String clientId = "23015113";
 
 	@Value("${top.appSecret}")
-	private final String clientSecret = "1ef1ff93070d9be52f3bdd05adc10a02";
+	private final String clientSecret = "c120077750c323108aebc0017832a188";
 
 	@Value("${top.oauth.token}")
 	private final String tokenUrl = "https://oauth.taobao.com/token";
